fb87f4bca51187ccf0f809560c3d69e29b453d72
MH TOUFIK <toufik@sopnolikhi.com>
2025-02-25 22:17:24 +0600

    Add Factorial program to calculate and display factorial of a number

---
+++ src/PCode/Factorial.java
@@ -0,0 +1,27 @@
+package PCode;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int fact = 1;
+
+        // for (int i = n; 0 < i; i--) {
+        // fact = fact * i;
+        // System.out.println(fact);
+        // }
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+            System.out.println(fact);
+        }
+
+        System.out.println("Factorial of " + n + "! = is " + fact);
+
+    }
+
+}
