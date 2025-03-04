eac341d5118de38f70271ce3b6fc3939f78fbb59
MH TOUFIK <toufik@sopnolikhi.com>
2025-03-04 19:23:26 +0600

    Enhance OddNumberPrint program to accept user-defined limit for odd number display

--- src/PCode/OddNumberPrint.java
+++ src/PCode/OddNumberPrint.java
@@ -1,14 +1,22 @@
 package PCode;
 
+import java.util.Scanner;
+
 public class OddNumberPrint {
     public static void main(String[] args) {
-        int limit = 100;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your limit of the odd number: ");
+        int limit = scanner.nextInt();
 
         System.out.println("Odd number list up to " + limit);
+
         for (int i = 0; i <= limit; i++) {
             if (i % 2 != 0) {
                 System.out.print(i + " ");
             }
         }
+
+        scanner.close();
     }
 }
