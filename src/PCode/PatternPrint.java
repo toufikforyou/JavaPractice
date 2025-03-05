43d3e47ebbe91d0095508bcf28bbb65e6a28a93b
MH TOUFIK <toufik@sopnolikhi.com>
2025-03-05 16:27:48 +0600

    Add methods to PatternPrint for star, number, and all number patterns

--- src/PCode/PatternPrint.java
+++ src/PCode/PatternPrint.java
@@ -4,7 +4,12 @@ public class PatternPrint {
 
     public static void main(String[] args) {
         int row = 6;
+        StarPrint(row);
+        NumberPrint(row);
+        AllNumberPrint(row);
+    }
 
+    public static void StarPrint(int row) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < 2 * (row - i) - 1; j++) {
                 System.out.print(" ");
@@ -12,6 +17,32 @@ public class PatternPrint {
 
             for (int k = 0; k < 2 * i + 1; k++) {
                 System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void NumberPrint(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < 2 * (row - i) - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void AllNumberPrint(int row) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < 2 * (row - i) - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print(k + " ");
             }
             System.out.println("");
         }
