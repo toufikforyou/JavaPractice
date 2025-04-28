2e2cb1d8ae49890e9d290e60cfd150bf84818926
MH TOUFIK <toufik@sopnolikhi.com>
2025-04-28 23:07:19 +0600

    MultiplyTwoNumbers add feature

---
+++ src/PCode/MultiplyTwoNumbers.java
@@ -0,0 +1,13 @@
+package PCode;
+
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
