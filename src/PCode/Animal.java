86994900a601aeae8986da422da19732998206bd
MH TOUFIK <toufik@sopnolikhi.com>
2025-05-02 00:04:19 +0600

    Animal class add

---
+++ src/PCode/Animal.java
@@ -0,0 +1,13 @@
+package PCode;
+
+public class Animal {
+
+    public void speak(String smg) {
+        System.out.println(smg);
+    }
+
+    public void color(String color) {
+        System.out.println(color);
+    }
+
+}
