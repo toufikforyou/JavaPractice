f083239b249b93efcf6934c59dbff27c2c40db88
MH TOUFIK <toufik@sopnolikhi.com>
2025-05-04 00:10:46 +0600

    program class added for overriting example.

---
+++ src/PCode/Program.java
@@ -0,0 +1,15 @@
+package PCode;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.speak("Ghew ghew");
+        dog.color("Black");
+
+        Cat cat = new Cat();
+        cat.speak("Maw maw");
+        cat.color("White");
+    }
+
+}
