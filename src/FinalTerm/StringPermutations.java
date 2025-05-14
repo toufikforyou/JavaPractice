d2743aff2b1e693f1753ca90b589cc09100253d4
MH TOUFIK <toufikforyou@gmail.com>
2025-05-14 20:52:17 +0600

    Refactor swap logic in StringPermutations class for improved readability

--- src/FinalTerm/StringPermutations.java
+++ src/FinalTerm/StringPermutations.java
@@ -2,6 +2,12 @@ package FinalTerm;
 
 public class StringPermutations {
 
+    public static void swap(char[] str, int l, int r) {
+        char temp = str[l];
+        str[l] = str[r];
+        str[r] = temp;
+    }
+
     public static void permute(char[] str, int l, int r) {
         if (l == r) {
             System.out.println(String.valueOf(str));
@@ -9,15 +15,11 @@ public class StringPermutations {
         }
 
         for (int i = l; i <= r; i++) {
-            char temp = str[i];
-            str[i] = str[l];
-            str[l] = temp;
+            swap(str, i, l);
 
             permute(str, l + 1, r);
 
-            temp = str[i];
-            str[i] = str[l];
-            str[l] = temp;
+            swap(str, i, l);
         }
     }
 
